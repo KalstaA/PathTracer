281ba992be0edd77bfd1bd3404ce6794f067c679
root <root@LAPTOP-1P1QVSV8>
2023-11-19 14:25:25 +0200

    updated fileloader to work with improved renderer

--- src/main.cpp
+++ src/main.cpp
@@ -12,48 +12,8 @@
 
 int main() {
 
-  Camera testCam = {
-    .position = Vector(0, 0, 0),
-    .direction = Vector(1, 0, 0),
-    .fov = M_PI / 3,
-    .up = Vector(0, 0, 1),
-    .focus_distance = 5
-  };  
-
-  Material RED_DIFFUSE = { .color = Color(1, 0, 0), .name = "RED DIFFUSE" };
-
-  Material GREY_DIFFUSE = { .color = Color(0.5, 0.5, 0.5), .name = "GREY DIFFUSE" };
-
-  Material GREEN_DIFFUSE = { .color = Color(0, 1, 0), .name = "GREEN DIFFUSE" };
-
-  Material BLUE_DIFFUSE = { .color = Color(0, 0, 1), .name = "BLUE DIFFUSE" };
-
-  Material MIRROR = { .specularity = 1.0, .name = "MIRROR" };
-
-  Material SUN = {
-    .color = Color(1, 1, 0),
-    .emission_color = Color(1, 1, 1),
-    .emission_strength = 0.5,
-    .name = "SUN"
-  };
-
-  Ball ball1(Vector(5, 0, 0), 1, RED_DIFFUSE);
-  Ball ball2(Vector(6, 0, -31), 30, GREY_DIFFUSE);
-  Ball ball3(Vector(10, -30, 20), 20, SUN);
-  Ball ball4(Vector(4, -0.5, -0.5), 0.5, GREEN_DIFFUSE);
-  Ball ball5(Vector(6, 2, -0.5), 0.5, BLUE_DIFFUSE);
-  Ball ball6(Vector(6, 1.5, 1), 0.7, MIRROR);
-
-  std::list<Object*> testBalls;
-  testBalls.push_back(&ball1);
-  testBalls.push_back(&ball2);
-  testBalls.push_back(&ball3);
-  testBalls.push_back(&ball4);
-  testBalls.push_back(&ball5);
-  testBalls.push_back(&ball6);
-
-  Scene testScene(testCam, testBalls);
-  testScene.getEnvironment().setSky();
+  FileLoader test("scene.yaml");
+  Scene testScene = test.loadSceneFile();
   std::cout << testScene;
 
   int resX = 500, resY = 400;
