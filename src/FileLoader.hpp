d7d6aef0e71d9e82ec82622b9f39ff9db9b2f128
Eetu Reijonen <eetujj.reijonen@gmail.com>
2023-11-19 16:10:00 +0200

    first working demo of the Path Tracer

--- src/FileLoader.hpp
+++ src/FileLoader.hpp
@@ -1,74 +1,149 @@
-/*#include <string>
-#include <fstream>  
-#include <iostream>
-#include <string>
-#include <../libs/Eigen/Dense>
-#include <yaml-cpp/yaml.h>
-#include "types.hpp"
-#include "ball.hpp"*/
 #pragma once
 
+#include <yaml-cpp/yaml.h>
+
+#include "ball.hpp"
+#include "scene.hpp"
+#include "types.hpp"
+
+/**
+ * @brief Implements a class for reading yaml scene files.
+ * 
+ */
 
 class FileLoader{
     public:
-        FileLoader(std::string filepath) : filepath_(filepath) {} //Constructs a FileLoader object with filepath to the scene file
 
-        Eigen::Vector3d LoadVector(YAML::Node coords) { //helper function to read and construct vectors
+        /**
+         * @brief Construct a new FileLoader object.
+         * 
+         * @param filepath Filepath to the yaml file that contains properties of the scene
+         */
+
+        FileLoader(std::string filepath) : filepath_(filepath) {} 
+
+        /**
+         * @brief Loads a scene from the file specified in filepath and returns an unique pointer to this scene.
+         * 
+         * @return A scene object
+         */
+        Scene loadSceneFile() {
+            YAML::Node params = YAML::LoadFile(filepath_);
+            Camera camera = LoadCamera();
+            std::list<Object*> objects = LoadObjects();
+            Scene loadedScene = Scene(camera, objects);
+            LoadEnvironment(loadedScene);
+            return loadedScene;
+        }
+
+        ~FileLoader() = default;
+
+        /**
+         * @brief Takes a reference to a scene object as an input and sets the environment of the scene to values specified in
+         * scene file. If scene file has no "Environment" node, the environment is set to default values.
+         * 
+         * @param scene A  reference to a scene object
+         */
+        void LoadEnvironment(Scene &scene) {
+            if(YAML::LoadFile(filepath_)["Environment"]) {
+                YAML::Node environment_node = YAML::LoadFile(filepath_)["Environment"];
+                Eigen::Vector3d skyColor = LoadVector(environment_node["SkyColor"]);
+                Eigen::Vector3d horizonColor = LoadVector(environment_node["HorizonColor"]);
+                Eigen::Vector3d groundColor = LoadVector(environment_node["GroundColor"]); 
+                scene.getEnvironment().setSky(skyColor, horizonColor, groundColor);
+            }else {
+                scene.getEnvironment().setSky();
+            }
+            
+        }
+
+    private:
+        /**
+         * @brief A helper function to create vectors from yaml sequences.
+         * 
+         * @param coords An yaml node that points to a key that has a sequence as a value 
+         * @return A vector based on values in node
+         */
+        Eigen::Vector3d LoadVector(YAML::Node coords) { 
             Eigen::Vector3d vector;
             int i = 0;
             for (YAML::const_iterator it=coords.begin(); it!=coords.end(); ++it) {
                 vector[i] = it->as<float>();
+                i++;
             }
             return vector;
         }
 
-        Material LoadMaterial(YAML::Node material_node) { //helper function to read and construct materials
+        /**
+         * @brief Creates a material struct based on properties given in yaml node.
+         * 
+         * @param material_node An yaml node that points to a key that has possible key-value pairs containing
+         * material values 
+         * @return A material struct  
+         */
+        Material LoadMaterial(YAML::Node material_node) { 
             Material material;
-            material.color = LoadVector(material_node["Color"]);
-            material.emission_color = LoadVector(material_node["EmissionColor"]);
-            material.emission_strength = material_node["EmissionStrength"].as<float>();
-            material.specularity = material_node["Specularity"].as<float>();
+            if(material_node["Color"]) {
+                material.color = LoadVector(material_node["Color"]);
+            }
+            if(material_node["EmissionColor"]) {
+             material.emission_color = LoadVector(material_node["EmissionColor"]);
+            }
+            if(material_node["EmissionStrength"]) {
+                material.emission_strength = material_node["EmissionStrength"].as<float>();
+            }
+            if(material_node["Specularity"]) {
+                material.specularity = material_node["Specularity"].as<float>();
+            }
+            if(material_node["Name"]) {
+                material.name = material_node["Name"].as<std::string>();
+            }
             return material;
         }
 
-        Ball LoadBall(YAML::Node ball) { //helper function to read and construct balls
-            return Ball(LoadVector(ball["Position"]), ball["Radius"].as<float>(), LoadMaterial(ball["Material"]));
+        /**
+         * @brief Creates a pointer to a ball object from yaml node.
+         * 
+         * @param ball Yaml node that contains properties of ball object
+         * @return A pointer to a ball object 
+         */
+        Object* LoadBall(YAML::Node ball) { 
+            Ball* ball_ptr = new Ball(LoadVector(ball["Position"]), ball["Radius"].as<float>(), LoadMaterial(ball["Material"]));
+            return ball_ptr;
         }
 
-        std::list<Ball> LoadBalls() { //helper function to load balls
+        /**
+         * @brief Loads all objects from yaml file 
+         * 
+         * @return A list of pointers to objects
+         */
+        std::list<Object*> LoadObjects() { 
             YAML::Node objects = YAML::LoadFile(filepath_)["Objects"];
-            std::list<Ball> ball_list;
-            int i = 1;
+            std::list<Object*> object_list;
             for (YAML::const_iterator it=objects.begin(); it!=objects.end(); ++it) {
-                if(it->as<std::string>() == ("Ball" + std::to_string(i))) {
-                    ball_list.push_back(LoadBall(objects["Ball" + std::to_string(i)]));
+                if((*it)["Object"]["Type"].as<std::string>() == "Ball") {
+                    object_list.push_back(LoadBall((*it)["Object"]));
                 }
-                i++;
             }
-            return ball_list;
+            return object_list;
         } 
 
-        Camera LoadCamera() {  //Loads the camera from YAML-file
-            YAML::Node params = YAML::LoadFile(filepath_);
+        /**
+         * @brief Creates a camera struct based on yaml file.
+         * 
+         * @return A camera struct 
+         */
+        Camera LoadCamera() {  
+            YAML::Node params = YAML::LoadFile(filepath_)["Camera"];
             Camera camera;
             camera.position = LoadVector(params["Position"]);
-            camera.position = LoadVector(params["Direction"]);
-            camera.position = LoadVector(params["Up"]);
-            camera.position = LoadVector(params["Right"]);
+            camera.direction = LoadVector(params["Direction"]);
+            camera.up = LoadVector(params["Up"]);
             camera.fov = M_PI * params["Fov"].as<float>();
             camera.focus_distance = params["FocusDistance"].as<float>();
             return camera;
         } 
 
-        Scene loadSceneFile() {
-            YAML::Node params = YAML::LoadFile(filepath_);
-            Camera camera = LoadCamera();
-            std::list<Ball> balls = LoadBalls();
-            return Scene(camera, balls);
-        }
 
-
-    private:
         std::string filepath_;
 };
-
