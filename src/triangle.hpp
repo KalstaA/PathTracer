7a518572bfa102ea99d3935e058503dbc77d3486
Eetu Reijonen <eetujj.reijonen@gmail.com>
2023-11-20 15:29:35 +0200

    triangle meshes merged

---
+++ src/triangle.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "object.hpp"
+#include <vector>
+
+
+struct Vertex 
+{
+public:
+    Vector pos;
+    Vector ng;
+    Vector2 uv;
+};
+
+
+class Triangle : public Object
+{
+public:
+    Triangle(Vertex v0, Vertex v1, Vertex v2, Material m) : Object(v0.pos, m) {
+        a = v0.pos;
+        b = v1.pos;
+        c = v2.pos;
+
+        e1 = b-a;
+        e2 = c-a;
+
+        n = e1.cross(e2).normalized();
+
+        uv[0] = v0.uv;
+        uv[1] = v1.uv;
+        uv[2] = v2.uv;
+
+        N[0] = v0.ng;
+        N[1] = v1.ng;
+        N[2] = v2.ng;
+    }
+
+    void collision(Ray& ray, Hit &rayHit, float& smallestDistance) {
+        Vector A1 = a-b;
+        Vector A2 = a-c;
+        Vector A3 = ray.direction;
+        Vector b = a-ray.origin;
+        
+        Matrix A;
+        A << A1, A2, A3;
+        double detA = A.determinant();
+
+        Matrix beta_A;
+        beta_A << b, A2, A3;
+        double beta = beta_A.determinant()/detA;
+
+        Matrix gamma_A;
+        gamma_A << A1, b, A3;
+        double gamma = gamma_A.determinant()/detA;
+
+        Matrix t_A;
+        t_A << A1, A2, b;
+        double t = t_A.determinant()/detA;
+        
+
+        if((beta + gamma < 1) && (beta > 0) && (gamma > 0) && t < smallestDistance) {
+            smallestDistance = t;
+            rayHit.distance = t;
+            rayHit.material = this->getMaterial();
+            rayHit.did_hit = true;
+            rayHit.point = ray.origin + ray.direction*t;
+            rayHit.normal = this->n;
+        }
+
+        return;
+    }
+
+    std::vector<Vector> getVertexPos() const {
+        std::vector<Vector> v;
+        v.push_back(a);
+        v.push_back(b);
+        v.push_back(c);
+        return v;
+    }
+
+    Vector getNormal() const {
+        return n;
+    }
+
+private:
+    Vector a, b, c;
+    Vector e1, e2;
+    Vector n;
+    Vector N[3];
+    Vector2 uv[3];
+};
+
+std::ostream &operator<<(std::ostream& out, const Triangle& triangle) {
+    std::vector<Vector> v = triangle.getVertexPos();
+    out << "Triangle at: (" << v[0].transpose() << "), (" << v[1].transpose() << "), (" << v[2].transpose() << "), with normal: (" << triangle.getNormal().transpose() << "), with material: " << triangle.getMaterial().name << std::endl;
+    return out;
+}
---
+++ src/trianglemesh.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "triangle.hpp"
+#include "../libs/tiny_obj_loader/tiny_obj_loader.cc"
+//#include "../libs/tiny_obj_loader/tiny_obj_loader.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+class TriangleMesh : public Object {
+public:
+    TriangleMesh(std::string obj_filepath, Vector scenePos, Material m) : Object(scenePos, m) {
+        unsigned long pos = obj_filepath.find_last_of("/");
+        std::string basepath = obj_filepath.substr(0, pos+1);
+
+        tinyobj::attrib_t attributes;
+        std::string warnings;
+        std::string errors;
+        std::vector<Vertex> vertices;
+
+        bool r = tinyobj::LoadObj(&attributes, &objects, &materials, &warnings, &errors, obj_filepath.c_str(), basepath.c_str());
+
+        if(!warnings.empty()) {
+            std::cout << "Warning: " << warnings << std::endl;
+        }
+
+        if(!errors.empty()) {
+            std::cout << "Error: " << errors << std::endl;
+        }
+
+        if(!r) {
+            std::cout << "Failed to load object file." << std::endl;
+            exit(1);
+        }
+
+        //Loop over objects (shapes)
+        for(size_t o = 0; o < objects.size(); o++) {
+            size_t o_offset = 0;
+            //Loop over triangles (faces)
+            for(size_t t = 0; t < objects[o].mesh.num_face_vertices.size(); t++) {
+                int fv = objects[o].mesh.num_face_vertices[t];
+
+                //Looping over vertices in this face
+                for(size_t v = 0; v < fv; v++) {
+                    tinyobj::index_t idx = objects[o].mesh.indices[o_offset + v];
+                    tinyobj::real_t vx = attributes.vertices[3*idx.vertex_index+0];
+                    tinyobj::real_t vy = attributes.vertices[3*idx.vertex_index+1];
+                    tinyobj::real_t vz = attributes.vertices[3*idx.vertex_index+2];
+                    tinyobj::real_t nx = attributes.normals[3*idx.normal_index+0];
+                    tinyobj::real_t ny = attributes.normals[3*idx.normal_index+1];
+                    tinyobj::real_t nz = attributes.normals[3*idx.normal_index+2];
+                    tinyobj::real_t tx = attributes.texcoords[2*idx.texcoord_index+0];
+                    tinyobj::real_t ty = attributes.texcoords[2*idx.texcoord_index+1];
+
+                    Vertex vrt = {
+                        .pos = Vector(vx, vy, vz)*0.01+scenePos,
+                        .ng = Vector(nx, ny, nz),
+                        .uv = Vector2(tx, ty)
+                    };
+                    vertices.push_back(vrt);
+                }
+                o_offset += fv;
+            }
+        }
+
+        //Loops over vertices and creates triangles
+        for(int i = 0; i < vertices.size()/3; ++i) {
+            triangles.push_back(std::make_shared<Triangle>(vertices[i*3], vertices[i*3+1], vertices[i*3+2], m));
+        }
+
+        std::cout << "Object file: " << obj_filepath << "succesfully opened!" << std::endl;
+
+        objects.clear();
+        materials.clear();
+    }
+
+    void collision(Ray& ray, Hit& rayHit, float& smallestDistance) {
+        long t_size = triangles.size();
+        for(long t = 0; t < t_size; t++){
+            triangles[t]->collision(ray, rayHit, smallestDistance);
+        }
+        return;
+    }
+
+public:
+    std::vector<tinyobj::shape_t> objects;
+    std::vector<tinyobj::material_t> materials;
+    std::vector<std::shared_ptr<Triangle>> triangles;
+};
